dd93d6a72cbc39c2cf27b9bf8579c200b13c0b4d
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-08-09 12:06:24 -0700

    Implemented Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { dateRetrieved } from "./RestaurantDisplay/deals"
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+const Header = () => (
+  <section className="flex flex-col justify-center items-center w-full">
+    <h1 className="text-center text-[72px] text-blue-500 font-bold italic">Fast Food Deals</h1>
+    <p className="text-center text-[24px] text-blue-300">Find all the best deals from popular fast food Restaurants</p>
+    <p className="text-center text-[24px] text-blue-300"><span className="font-semibold underline">Deals Last Updated:</span> {dateRetrieved}</p>
+  </section>
+)
 
 export default Header
