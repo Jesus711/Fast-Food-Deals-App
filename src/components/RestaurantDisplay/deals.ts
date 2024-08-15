a0689be13d738454960e7afa59d8fa8cec1a7dbe
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-08-15 14:30:48 -0700

    Latest retrieval of Deals

--- src/components/RestaurantDisplay/deals.ts
+++ src/components/RestaurantDisplay/deals.ts
@@ -1,8 +1,8 @@
-//Last Retrieved: Fri Aug 09 2024
+//Last Retrieved: Thu Aug 15 2024
 
 import { Deals } from "../../interfaces/Deal";
 
-export const dateRetrieved = "Aug 9, 2024";
+export const dateRetrieved = "Aug 15, 2024";
 
 const deals: Deals = {
   "Little Caesars": [
@@ -23,170 +23,213 @@ const deals: Deals = {
     },
     {
       "text": "PREMIUM ADD-ON!HALF OFF CRAZY PUFFS®WITH ANY TWO PIZZAS",
-      "image": "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+      "image": "https://littlecaesars.com/_next/image//?url=https%3A%2F%2Fbanners.littlecaesars.com%2Fus%2Fbanner-assets%2Fdeal-PUFFSDEAL.jpg&w=3840&q=75",
       "id": 3
     }
   ],
   "Burger King": [
     {
       "id": 0,
+      "text": "Free King Jr.™ Meal with Minimum Purchase of $10  King Jr.™ Meal (Cheeseburger, Hamburger, 6 Pc. Nuggets, or 4 Pc. Nuggets)",
+      "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/1a1d5079d28192b6be199df4d9566f83c055b112-1333x1333.png?w=187&q=80&fit=max&auto=format",
+      "url": "https://www.bk.com/rewards/offers/5fa7d84e-8c0d-4442-b4db-a7172763f07c"
+    },
+    {
+      "id": 1,
       "text": "Free Any Size Fries with Minimum Purchase of $1",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/09609532f7bfffd2046fc4f8015c8d0c538a570f-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/49b9d02e-e4a4-4e8e-a622-2085102f4bcf"
     },
     {
-      "id": 1,
+      "id": 2,
       "text": "$30 - Ultimate Bundle 2x Whopper Sandwiches + 2x OCS + 1x 16 Pc. Nuggets + 2x Small Fries + 2x Small Drinks",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/b8a430eb5d86945fcc070fd850fc5e06e136945c-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/eaa669ac-3318-4a96-b598-cf302afeb280"
     },
     {
-      "id": 2,
+      "id": 3,
       "text": "$22 - Sandwiches & Chicken Nuggets for 4 2x Whopper Sandwiches + 2x Original Chicken Sandwiches + 2x 8 Pc. Chicken Nuggets",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/ca0fa8006f3243071916edecbfdd9e5c15b03e97-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/b7bcb2ed-f26a-4347-8b19-f693b8464dd4"
     },
     {
-      "id": 3,
+      "id": 4,
       "text": "BOGO Original Chicken Sandwich Buy One Original Chicken Sandwich, Get One Free",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/fcee1acaa26032b6a44f585e834393dabfa232e3-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/9de2ad55-05f3-4b6b-a276-743d5fd6c7c2"
     },
     {
-      "id": 4,
+      "id": 5,
       "text": "$15.99 - Whopper Meal for Two 2x Whopper Sandwiches + 2x Small Fries + 2x Small Soft Drinks",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/8915166ff9ac6a2cc4ee9b56010ddc09110f2aec-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/ccf8ee66-81d6-4ab3-9f39-4d2d9a027940"
     },
     {
-      "id": 5,
+      "id": 6,
       "text": "$10.99 - Bacon King & Fries 1x Bacon King + 1x Small Fries",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/5c5b70ccf8231a2e523da40c04f97c97efcb5add-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/baecec2d-8fde-40b1-a1f5-1b2b271982ab"
     },
     {
-      "id": 6,
+      "id": 7,
       "text": "$8.99 - Whopper Jr. Sandwiches & Fries 2x Whopper Jr. Sandwiches + 2x Small Fries",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/cf9718520c5e64255a859b2bc4fe239bc0349735-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/513e9827-bc1b-4284-ab73-a207a53c374b"
     },
     {
-      "id": 7,
+      "id": 8,
       "text": "$9.99 - Double Whopper & Fries 1x Double Whopper + 1x Small Fries",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/a396930522c870d97a831a9c058702047e630334-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/2089cfb1-9b8b-4985-a53b-b19311b00f50"
     },
     {
-      "id": 8,
+      "id": 9,
       "text": "$8.99 - Whopper Meal 1x Whopper +1x Small Fries + 1x Small Soft Drink",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/7e2fc1e3f01b46c9860338c4747e4f41e32ba8e0-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/00f80bbc-02f5-4d3f-9b8a-641df18065f5"
     },
     {
-      "id": 9,
+      "id": 10,
       "text": "$7.99 - Royal Crispy Chicken Meal 1x Royal Crispy Chicken +1x Small Fries + 1x Small Soft Drink",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/17151de9a2057f298f85dc4bee5e737fbb46ea55-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/bdfb326e-d622-430f-b6f9-9767fbb6c8ab"
     },
     {
-      "id": 10,
+      "id": 11,
       "text": "$6.99 - Chicken Fries & Fries 1x 8 Pc. Chicken Fries + 1x Large Fries",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/134efc9eef3d98d9c01be4d8114a8704387a81a1-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/69a02c94-1059-4b0a-89ee-4d36f3e8bbf1"
     },
     {
-      "id": 11,
+      "id": 12,
       "text": "$5.99 - Double Cheeseburger & Fries 1x Double Cheeseburger + 1x Small Fries",
       "image": "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/a4147d2bcebe1eaa4a56710700a427ba6b76f1c1-1333x1333.png?w=187&q=80&fit=max&auto=format",
       "url": "https://www.bk.com/rewards/offers/0ddb1476-85cc-4136-bbaa-930517533c3a"
     }
   ],
+  "Wendy's": [
+    {
+      "id": 0,
+      "text": "Bacon Double Stack™ Biggie™ Bag $9.00",
+      "image": "https://app.wendys.com/unified/assets/menu/pg-cropped/1759_small_US_en.png",
+      "url": "https://order.wendys.com/product/31473?lang=en_US"
+    },
+    {
+      "id": 1,
+      "text": "Crispy Chicken BLT Biggie™ Bag $8.00",
+      "image": "https://app.wendys.com/unified/assets/menu/pg-cropped/1760_small_US_en.png",
+      "url": "https://order.wendys.com/product/31474?lang=en_US"
+    },
+    {
+      "id": 2,
+      "text": "Crispy Chicken Biggie™ Bag $7.00",
+      "image": "https://app.wendys.com/unified/assets/menu/pg-cropped/1763_small_US_en.png",
+      "url": "https://order.wendys.com/product/31475?lang=en_US"
+    },
+    {
+      "id": 3,
+      "text": "Jr. Bacon Cheeseburger Biggie™ Bag $8.00",
+      "image": "https://app.wendys.com/unified/assets/menu/pg-cropped/1761_small_US_en.png",
+      "url": "https://order.wendys.com/product/31476?lang=en_US"
+    },
+    {
+      "id": 4,
+      "text": "Double Stack™ Biggie™ Bag $8.00",
+      "image": "https://app.wendys.com/unified/assets/menu/pg-cropped/1762_small_US_en.png",
+      "url": "https://order.wendys.com/product/31477?lang=en_US"
+    },
+    {
+      "id": 5,
+      "text": "Jr. Cheeseburger – 444 Meal $7.00",
+      "image": "https://app.wendys.com/unified/assets/menu/pg-cropped/1098_small_US_en.png",
+      "url": "https://order.wendys.com/product/31109?lang=en_US"
+    }
+  ],
   "Jack In The Box": [],
-  "Wendy's": [],
   "KFC": [
     {
       "id": 0,
-      "text": "GREAT VALUE Taste of KFC 2 pc. Drums Deal Cal.: 570 $4.99",
+      "text": "GREAT VALUE Taste of KFC 2 pc. Drums Deal Cal.: 570 $5.00",
       "url": "https://www.kfc.com/menu/featured/taste-of-kfc-2-drums-deal",
       "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-TasteofKFCValueCA2pc.webp?q=75"
     },
     {
       "id": 1,
+      "text": "GREAT VALUE Taste of KFC 8 pc. Nuggets Deal Cal.: 590-850 $5.00",
+      "url": "https://www.kfc.com/menu/featured/taste-of-kfc-8-piece-nuggets-deal",
+      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-TasteofKFC8pcNuggets.webp?q=75"
+    },
+    {
+      "id": 2,
+      "text": "GREAT VALUE Taste of KFC Famous Bowl Deal Cal.: 590 $5.00",
+      "url": "https://www.kfc.com/menu/featured/taste-of-kfc-famous-bowl",
+      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-FamousBowlNuggetsV2.webp?q=75"
+    },
+    {
+      "id": 3,
       "text": "Taste of KFC 4 pc. Drums & Thighs Deal Cal.: 1080-1620 $12.00",
       "url": "https://www.kfc.com/menu/featured/taste-of-kfc-4-piece-drums-and-thighs-deal",
       "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-TasteofKFCValueCA4pc.webp?q=75"
     },
     {
-      "id": 2,
+      "id": 4,
       "text": "GREAT VALUE Taste of KFC 6 pc. Deal Cal.: 4110-4470 $20.00",
       "url": "https://www.kfc.com/menu/featured/taste-of-kfc-6-piece-deal",
       "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-6pcTasteofKFCMeal.webp?q=75"
     },
     {
-      "id": 3,
+      "id": 5,
       "text": "8 pc. Original Recipe Drumsticks Cal.: 1040 $10.00",
       "url": "https://www.kfc.com/menu/featured/8-piece-original-drums-bucket",
       "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-8pcORDrumsBucketv2.webp?q=75"
     },
     {
-      "id": 4,
-      "text": "10 pc. Saucy Nuggets Combo Cal.: 920-1220 $9.99",
-      "url": "https://www.kfc.com/menu/featured/10-piece-saucy-nuggets-combo",
-      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-10pcHBBQNuggetsCombo.webp?q=75"
-    },
-    {
-      "id": 5,
-      "text": "10 pc. Saucy Nuggets Cal.: 420-430 $5.99",
-      "url": "https://www.kfc.com/menu/featured/10-piece-saucy-nuggets",
-      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-10pcHBBQNuggets.webp?q=75"
-    },
-    {
       "id": 6,
-      "text": "DIGITAL EXCLUSIVE Saucy Chicken Sandwich Combo Cal.: 920-1230 $9.99",
-      "url": "https://www.kfc.com/menu/featured/saucy-sandwich-combo",
-      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-HBBQSandwichCombo.webp?q=75"
+      "text": "NEW 10 pc. Saucy Nuggets Combo Cal.: 900-1330 $9.99",
+      "url": "https://www.kfc.com/menu/featured/10-piece-saucy-nuggets-combo",
+      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-10pcHonGarlicNuggetsCombo.webp?q=75"
     },
     {
       "id": 7,
-      "text": "DIGITAL EXCLUSIVE Saucy Chicken Sandwich Cal.: 600-620 $5.99",
-      "url": "https://www.kfc.com/menu/featured/saucy-sandwich",
-      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-HBBQSandwich.webp?q=75"
+      "text": "NEW 10 pc. Saucy Nuggets Cal.: 400-540 $5.99",
+      "url": "https://www.kfc.com/menu/featured/10-piece-saucy-nuggets",
+      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-10pcHonGarlicNuggets.webp?q=75"
     },
     {
       "id": 8,
+      "text": "DIGITAL EXCLUSIVE Saucy Chicken Sandwich Combo Cal.: 920-1280 $9.99",
+      "url": "https://www.kfc.com/menu/featured/saucy-sandwich-combo",
+      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-HonGarlicSandwichCombo.webp?q=75"
+    },
+    {
+      "id": 9,
+      "text": "DIGITAL EXCLUSIVE Saucy Chicken Sandwich Cal.: 590-670 $5.99",
+      "url": "https://www.kfc.com/menu/featured/saucy-sandwich",
+      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-HonGarlicSandwich.webp?q=75"
+    },
+    {
+      "id": 10,
       "text": "Apple Pie Poppers Cal.: 340-850 $2.49",
       "url": "https://www.kfc.com/menu/featured/apple-pie-poppers",
       "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-4pcPiePoppers.webp?q=75"
     },
     {
-      "id": 9,
+      "id": 11,
       "text": "Tropical Passionfruit Lemonade Cal.: 310-990 $2.99",
       "url": "https://www.kfc.com/menu/featured/tropical-passion-fruit-lemonade",
       "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-PassionfruitLemonade.webp?q=75"
     },
     {
-      "id": 10,
-      "text": "SPECIAL OFFER Fill Up Box Cal.: 2600-3630 $23.00",
+      "id": 12,
+      "text": "Fill Up Box Cal.: 2600-3630 $23.00",
       "url": "https://www.kfc.com/menu/featured/fill-up-box",
       "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-FillUpBox.webp?q=75"
     },
     {
-      "id": 11,
+      "id": 13,
       "text": "DIGITAL EXCLUSIVE 8 pc. Chicken + 8 Tenders Fill Up Cal.: 5080-6800 $40.00",
       "url": "https://www.kfc.com/menu/featured/8-piece-chicken-and-8-tenders-meal",
       "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-8pcBucket&8TendersBucket.webp?q=75"
-    },
-    {
-      "id": 12,
-      "text": "DIGITAL EXCLUSIVE 50 pc. Nuggets Cal.: 2120-2970 $25.00",
-      "url": "https://www.kfc.com/menu/featured/50-piece-nuggets",
-      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-50pcNuggets.webp?q=75"
-    },
-    {
-      "id": 13,
-      "text": "DIGITAL EXCLUSIVE Chicken + Nuggets Family Meal Cal.: 2510-4760 $24.99",
-      "url": "https://www.kfc.com/menu/featured/chicken-and-nuggets-family-meal",
-      "image": "https://digitaleat.kfc.com/menus/image/bare/kfc-6pcCOB12pcNuggets-DigEx.webp?q=75"
     }
   ],
   "Wingstop": [],
