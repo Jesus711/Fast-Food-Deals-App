3c43206c40abb8097c86b2cd369f69761dd7083f
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-08-15 15:22:20 -0700

    Updated Header component to show the area searched for

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
-import { dateRetrieved } from "./RestaurantDisplay/deals"
+import { dateRetrieved, area } from "./RestaurantDisplay/deals"
 
 const Header = () => (
   <section className="flex flex-col justify-center items-center w-full">
     <h1 className="text-center text-[72px] text-blue-500 font-bold italic">Fast Food Deals</h1>
     <p className="text-center text-[24px] text-blue-300">Find all the best deals from popular fast food Restaurants</p>
-    <p className="text-center text-[24px] text-blue-300"><span className="font-semibold underline">Deals Last Updated:</span> {dateRetrieved}</p>
+    <p className="text-center text-[24px] text-blue-300">Deals from: <span className="font-semibold underline">{area}</span></p>
+    <p className="text-center text-[24px] text-blue-300">Last Updated: <span className="font-semibold underline">{dateRetrieved}</span> </p>
   </section>
 )
 
